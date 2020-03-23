ed1edcfbee7ea3fda62e2574a4233082692d6e32
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-03-23 13:19:53 -0400

    Finished project

--- js/end.js
+++ js/end.js
@@ -4,14 +4,29 @@ const finalScore = document.getElementById('finalScore');
 
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
+console.log(highScores);
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-  console.log(username.value);
   saveScoreButton.disabled = !username.value;
 });
 
 saveHighScore = e => {
   e.preventDefault();
-  console.log('clicked the save button');
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  };
+
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+  window.location.assign('/');
 };
